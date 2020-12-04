3f4657d0e81b26ce6bb077da060fe06fb04b4f53
ibkov <34207589+ibkov@users.noreply.github.com>
2020-12-04 13:59:42 +0300

    first_commit

---
+++ cmake-build-debug/CMakeFiles/FT_PRINTF.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/FT_PRINTF.dir/main.c.o"
+  "FT_PRINTF"
+  "FT_PRINTF.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/FT_PRINTF.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
